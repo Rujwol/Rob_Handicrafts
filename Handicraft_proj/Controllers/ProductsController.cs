bb9b5444afbccba3b6d20da7034b27d28363bed6
Rujwol <rujwol.7@gmail.com>
2023-10-10 01:14:01 +0545

    Initial commit Landing page

---
+++ Handicraft_proj/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Handicraft_proj.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult AllProducts()
+        {
+            return View();
+        }
+    }
+}
---
+++ Handicraft_proj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Handicraft_proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
